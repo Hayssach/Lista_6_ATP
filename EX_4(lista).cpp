3992f4e3dbb2b73fb6fa244a924ad19638241a66
Hayssa Fonseca <hayssa.fonseca40@gmail.com>
2025-07-07 14:02:54 -0300

    EX_4(lista).cpp

--- EX_4(lista).cpp
+++ EX_4(lista).cpp
@@ -1,47 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Definição da função potenciaRecursiva
-double potenciaRecursiva(double base, int expoente) {
-    // Caso base: Se o expoente for 1, base^1 é a própria base.
-    if (expoente == 1) {
-        return base;
+// FunÃ§Ã£o potenciaRecursiva
+double potenciaRecursiva(double b, int exp) {
+    // Caso b: Se o exp for 1, b^1 Ã© o prÃ³prio b.
+    if (exp == 1) {
+        return b;
     }
-    // Passo recursivo: base^expoente = base * base^(expoente-1)
+    // base^expoente = base * base^(expoente-1)
     else {
-        if (expoente <= 0) {
-             return base * potenciaRecursiva(base, expoente - 1);
+        if (exp <= 0) {
+             return b * potenciaRecursiva(b, exp - 1);
         }
-        return base * potenciaRecursiva(base, expoente - 1);
+        return b * potenciaRecursiva(b, exp - 1);
     }
 }
 
 int main() {
-    double baseUsuario;
-    int expoenteUsuario;
+    double bUsuario;
+    int expUsuario;
 
     printf("Digite o valor da base (ex: 2.5): ");
-    if (scanf("%lf", &baseUsuario) != 1) {
+    if (scanf("%lf", &bUsuario) != 1) {
         printf("Erro ao ler o valor da base.\n");
         return 1; // Termina o programa indicando um erro
     }
 
-    printf("Digite o valor do expoente (um número inteiro positivo, ex: 3): ");
-    if (scanf("%d", &expoenteUsuario) != 1) {
+    printf("Digite o valor do expoente (um nÃºmero inteiro positivo, ex: 3): ");
+    if (scanf("%d", &expUsuario) != 1) {
         printf("Erro ao ler o valor do expoente.\n");
-        return 1; // Termina o programa indicando um erro
+        return 1; 
     }
 
-    // Verifica se o expoente é positivo, conforme a premissa da função potenciaRecursiva
-    if (expoenteUsuario <= 0) {
-        printf("Erro: O expoente deve ser um número inteiro positivo.\n");
-        return 1; // Termina o programa indicando um erro
+    // Verifica se o expoente Ã© positivo
+    if (expUsuario <= 0) {
+        printf("Erro: O expoente deve ser um nÃºmero inteiro positivo.\n");
+        return 1; 
     }
 
-    double resultado = potenciaRecursiva(baseUsuario, expoenteUsuario);
+    double r = potenciaRecursiva(baseUsuario, expoenteUsuario);
 
-    // Exibe o resultado
-    printf("%.2f elevado a %d é %.2f\n", baseUsuario, expoenteUsuario, resultado);
+
+    printf("%.2f elevado a %d Ã© %.2f\n", bUsuario, expUsuario, r);
 
     return 0; 
 }
