b999d5789f512d7df5cd5b5264931063699f6ed4
Hayssa Fonseca <hayssa.fonseca40@gmail.com>
2025-07-07 13:55:51 -0300

    EX_1(lista).cpp

--- EX_1(lista).cpp
+++ EX_1(lista).cpp
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <time.h> // biblioteca time
 
-// Função recursiva que soma 'n' números aleatórios de 1 a 6
-int soma_recursiva(int k) {
-    if (k == 0)
+// FunÃ§Ã£o recursiva que soma 'n' 
+int somar_recursiva(int f) {
+    if (f == 0)
         return 0;
-    return (rand() % 6 + 1) + soma_recursiva(k - 1);
+    return (rand() % 6 + 1) + somar_recursiva(f - 1);
 }
 
-int numero_aleatorio_2_a_12() {
-    return soma_recursiva(2);
+int num_aleatorio_2_a_12() {
+    return somar_recursiva(2);
 }
 
 int main() {
     srand(time(NULL));
 
-    int numero = numero_aleatorio_2_a_12();
-    printf("Número aleatório entre 2 e 12: %d\n", numero);
+    int num = num_aleatorio_2_a_12();
+    printf("NÃºmero aleatÃ³rio entre 2 e 12: %d\n", num);
 
     return 0;
 }
